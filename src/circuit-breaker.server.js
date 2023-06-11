0fb0f085ff3ca3dd30807d784ea02918da4265a3
Leonardo Anjos <leodrums11@gmail.com>
2023-06-11 16:57:50 -0300

    chore: remove script start application

--- src/circuit-breaker.server.js
+++ src/circuit-breaker.server.js
@@ -6,13 +6,12 @@ const fetchDataFromExternalVendor = async () => {
 }
 
 // sample request to fetch data asynchronously
-// const request = axios fetchDataFromExternalVendor();
 const request = fetchDataFromExternalVendor();
 
-// // wrap the request within a circuit breaker object
-// const circuitBreakerObject = new CircuitBreaker(request, { failureThreshold: 4, timeout: 4000 });
+// wrap the request within a circuit breaker object
+const circuitBreakerObject = new CircuitBreaker(request, { failureThreshold: 4, timeout: 4000 });
 
-// // fire the request
-// circuitBreakerObject.fire()
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(`some error occurred = ${err.message}`))
+// fire the request
+circuitBreakerObject.fire()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(`some error occurred = ${err.message}`))
